0134ceb59442913da0d183b3903fe6c7c7118536
andy12425 <andersonbeauvais1@hotmail.fr>
2020-02-08 15:27:17 -0500

    Resquet axios pour connecter un utilisateur

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View,ScrollView,BackHandler} from 'react-native';
+import { StyleSheet, Text,TextInput, View,ScrollView,BackHandler} from 'react-native';
 import Header from './component/header'
 import { render } from 'react-dom';
 import axios from 'react-native-axios';
@@ -9,10 +9,38 @@ import Footer from './component/footer'
 import { Input,Button} from 'react-native-elements';
 
 export default class App extends React.Component {
- 
+  
+state={
+  username:"",
+  password:"",
+  currentClient:{},
+  load:false
+
+}
   
   render(){
   
+  confirm=> {
+    const username = this.state.username;
+    const password = this.state.password; 
+    this.setState({load:true})
+    if(username.length>4&& password.length> 6 ){
+ 
+      axios.get('http://localhost/apibanque/user/find_One.php',{username:username,password:password })
+      .then(res=> {
+        const client = res.data;
+        this.setState({currentClient:client})
+        this.setState({load:false})
+
+      })
+
+
+
+    }
+    
+
+  }
+  console.log(this.state.username);
     
   return (
     <View style={{ flex: 1 }} >
@@ -20,9 +48,9 @@ export default class App extends React.Component {
      <Header subText="Login"></Header>
 </View>
 <View style={styles.loginbody}>
-<Input placeholder="username"></Input>
-<Input placeholder="Password" onChange ></Input>
-<Button  title="Connection"></Button>
+<TextInput name="username" onChangeText={text=> this.setState({username:text})}  placeholder="username"></TextInput>
+<TextInput name="password" placeholder="Password" onChangeText={text=> this.setState({password:text})} ></TextInput>
+<Button  title="Connection" loading={this.state.load}></Button>
 
 </View>
 
